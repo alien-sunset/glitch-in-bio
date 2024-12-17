564cac5af2f2cbbb321b21b74537a14080c5d555
Alien Sunset <89020995+alien-sunset@users.noreply.github.com>
2024-12-17 18:45:38 -0500

    Add files via upload
    how tf do workflows and glitc exports work?

---
+++ templates/header.js
@@ -0,0 +1,10 @@
+import { html } from "lighterhtml";
+import settings from "../settings.json";
+
+export const renderHeader = html.node`<div>
+  <div class='avatar-container'>
+    <img src='${settings.avatarImage}' class='avatar' alt='${settings.name}' />
+  </div>
+  <h1>${settings.name}</h1>
+  <h3>${settings.subtitle}</h3>
+</div>`;
